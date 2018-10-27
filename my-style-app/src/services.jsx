6824a46005a391b21831d7eae641885ec7767b62
unknown <mabbasidesign@yahoo.com>
2018-10-26 22:27:10 -0600

    complete services

--- my-style-app/src/services.jsx
+++ my-style-app/src/services.jsx
@@ -9,30 +9,60 @@ class Services extends Component {
                     <h3>Services</h3>
                     <h2>What We Offer</h2>
                     <div className="row">
-                        <div>
-                            <span><ion-icon name="phone-portrait"></ion-icon></span>
-                            <h4>Responsive</h4>
-                            <p>Looks great on any screen size</p>
-                        </div>
-                        <div>
-                            <span><ion-icon name="brush"></ion-icon></span>
-                            <h4>Redesign</h4>
-                            <p>Freshly redesigned</p>
-                        </div>
-                        <div>
-                            <span><ion-icon name="thumbs-up"></ion-icon></span>
-                            <h4>Favorite</h4>
-                            <p>Milion of users</p>
-                        </div>
-                        <div>
-                            <span><ion-icon name="help"></ion-icon></span>
-                            <h4>Question</h4>
-                            <p>I mustache you a question...</p>
-                        </div>
+
+                        <Icons icon={iconObj[0].icon} title={iconObj[0].title} description={iconObj[0].description} />
+
+                        <Icons icon={iconObj[1].icon} title={iconObj[1].title} description={iconObj[1].description} />
+
+                        <Icons icon={iconObj[2].icon} title={iconObj[2].title} description={iconObj[2].description} />
+
+                        <Icons icon={iconObj[3].icon} title={iconObj[3].title} description={iconObj[3].description} />
+
                     </div>
                 </div>
          );
     }
 }
  
+
+const iconObj = [
+    {
+        icon:<ion-icon name="phone-portrait"></ion-icon>,
+        title: 'Responsive',
+        description: 'Looks great on any screen size'
+    },
+    {
+        icon:<ion-icon name="brush"></ion-icon>,
+        title: 'Redesign',
+        description: 'Freshly redesigned'
+    },
+    {
+        icon:<ion-icon name="thumbs-up"></ion-icon>,
+        title: 'Favorite',
+        description: 'Milion of users'
+    },
+    {
+        icon:<ion-icon name="help"></ion-icon>,
+        title: 'Question',
+        description: 'I mustache you a question...'
+    },
+];
+
+class Icons extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>
+                <div>
+                    <span>{this.props.icon}</span>
+                    <h4>{this.props.title}</h4>
+                    <p>{this.props.description}</p>
+                </div>
+            </div>
+         );
+    }
+}
+ 
+// export default Icon;
+
 export default Services;
